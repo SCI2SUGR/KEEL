401f077dc9950ab2d93173bbad4b7bdc96fa9084
Joaquin Derrat <jderrac@decsai.ugr.es>
2010-04-29 10:10:59 +0000

    Fin de la importacion del modulo de estadistica

--- src/keel/Algorithms/Statistical_Tests/Regression/Friedman/Main.java
+++ src/keel/Algorithms/Statistical_Tests/Regression/Friedman/Main.java
@@ -1,6 +1,6 @@
 /**
 * <p>
-* @author Written by Alberto Fernandez (University of Granada)01/01/2008
+* @author Written by Joaquín Derrac (University of Granada)26/04/2008
 * @version 1.0
 * @since JDK1.5
 * </p>
@@ -9,22 +9,9 @@ package keel.Algorithms.Statistical_Tests.Regression.Friedman;
 
 import keel.Algorithms.Statistical_Tests.Shared.*;
 import keel.Algorithms.Shared.Parsing.*;
-import org.core.*;
 
 public class Main {
-	/**
-	* <p>
-	* This class has only a main method that calls Friedman test for regression problems, defined in StatTest
-	* </p>
-	*/
 	
-	/**
-	* <p>
-	* This method reads a configuration file and calls statisticalTest with appropriate values to run the
-	* Friedman test for regression problems, defined in StatTest class
-	* @param args A string that contains the command line arguments
-	* </p>
-	*/
     public static void main(String args[]) {
 
         boolean tty = false;
@@ -33,7 +20,6 @@ public class Main {
         if (pc.fileProcess(args[0]) < 0) {
             return;
         }
-        int algorithm = pc.parAlgorithmType;
 
         ParseFileList pl = new ParseFileList();
 
